c9244e24b146f06fe709e8d44fca62b123c51d3b
Midacts <midactsmystery@gmail.com>
2013-04-05 17:42:55 -0300

    Update RenamePC.ps1

--- RenamePC.ps1
+++ RenamePC.ps1
@@ -3,7 +3,7 @@ $newname = "COMPUTERNAME"
 $user = "USERNAME"
 $pwd = "TYPE PWD"
 
-function rar([string]$computer, [string]$newname)
+function renamePC([string]$computer, [string]$newname)
 {
         $comp = gwmi win32_computersystem  -computer $computer -authentication 6
         $os   = gwmi win32_operatingsystem -computer $computer
